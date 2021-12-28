072f3054391ba926a9dc6a65a0e8c068875754ce
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2021-12-28 22:07:06 +0530

    homepage done, services done, projects 70percent done

--- my website/pdt.js
+++ my website/pdt.js
@@ -1,11 +1,22 @@
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } 
-  else {
-    x.className = "topnav";
-  }
-}
-</script>
+<div class="navbar fixed-top" id="navbar">
+<div class="logo">py_dev_tahzeeb</div>
+<div class="nav-links">
+  |
+  <li><a href="#Home">Home</a></li>
+  |
+  <li><a href="#About">About</a></li>
+  |
+  <li><a href="#Skills">Skills</a></li>
+  |
+  <li><a href="#Services">Services</a></li>
+  |
+  <li><a href="#Projects">Projects</a></li>
+  |
+  <li><a href="#blogs">Blogs</a></li>
+  |
+  <li><a href="#Testomonial">Testomonial</a></li>
+  <li><a href="#ContactMe" class="contactme-top">Contact Me</a></li>
+  
+
+</div>
+</div>
