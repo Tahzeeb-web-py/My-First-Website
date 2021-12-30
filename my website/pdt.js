67d123ee97d76ebf8cb2a8c7a6e0161cbee4a886
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2021-12-30 17:42:55 +0530

    My Website work is over, Now it is Ready to Host on the Internet

--- my website/pdt.js
+++ my website/pdt.js
@@ -1,22 +1,67 @@
-<div class="navbar fixed-top" id="navbar">
-<div class="logo">py_dev_tahzeeb</div>
-<div class="nav-links">
-  |
-  <li><a href="#Home">Home</a></li>
-  |
-  <li><a href="#About">About</a></li>
-  |
-  <li><a href="#Skills">Skills</a></li>
-  |
-  <li><a href="#Services">Services</a></li>
-  |
-  <li><a href="#Projects">Projects</a></li>
-  |
-  <li><a href="#blogs">Blogs</a></li>
-  |
-  <li><a href="#Testomonial">Testomonial</a></li>
-  <li><a href="#ContactMe" class="contactme-top">Contact Me</a></li>
-  
-
-</div>
-</div>
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  });
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+      strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  var typed = new Typed(".typing-2", {
+      strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  // owl carousel script
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
+});
